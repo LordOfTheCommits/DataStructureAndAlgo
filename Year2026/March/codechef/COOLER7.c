fbd92c8d0d6493fd9908d8cc814ed80d3c076086
Zuhair Arshad <zuhairarshad05@gmail.com>
2026-04-24 16:17:01 +0530

    pushing my first codechef code for tracking in future

---
+++ Year2026/March/codechef/COOLER7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int T;   // number of test cases
+    scanf("%d", &T);
+
+    while (T--) {
+        int N, M,sum;
+        scanf("%d %d", &N, &M);
+    
+sum = ((N * (N + 1))  - (M * (M + 1))) / 2;
+printf("%d\n",sum);
+    }
+
+    return 0;
+}
